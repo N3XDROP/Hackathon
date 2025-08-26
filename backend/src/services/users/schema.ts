336ce3c306fe782fa0ee08cfc89393a4e24e697f
José Luis Torres Rivera <torresriverajoseluis9@gmail.com>
2025-08-26 16:58:44 -0500

    Se eliminan los roles del esquema del usuario

--- backend/src/services/users/schema.ts
+++ backend/src/services/users/schema.ts
@@ -1,11 +1,9 @@
 import { z } from "zod";
-import { Roles } from "../utils/enums";
 
 export const UserSchema = z.object({
 	email: z.string().email().max(150),
     name: z.string().max(150),
 	password: z.string().min(8).max(150),
-	role: z.nativeEnum(Roles).optional().default(Roles.USER),
 	resetToken: z.string().optional().default(""),
 	refreshToken: z.string().optional().default(""),
 });
