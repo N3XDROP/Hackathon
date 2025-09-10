04f68f7ad25b34dfaf38aeef42f6331cc729701e
N3XDROP <julian.peraltac@gmail.com>
2025-09-09 20:55:05 -0500

    Frontend/Backend deleted

--- backend/src/services/users/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const UserSchema = z.object({
-	email: z.string().email().max(150),
-    name: z.string().max(150),
-	password: z.string().min(8).max(150),
-	resetToken: z.string().optional().default(""),
-	refreshToken: z.string().optional().default(""),
-});
